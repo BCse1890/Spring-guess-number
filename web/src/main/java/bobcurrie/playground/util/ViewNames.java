f76e67447d1a8f7dad7f48fcacbdea52da120039
BCse1890 <bob.currie@fsresearch.co.uk>
2018-11-17 21:54:13 +0000

    Add Spring Boot to project and devtools

---
+++ web/src/main/java/bobcurrie/playground/util/ViewNames.java
@@ -0,0 +1,10 @@
+package bobcurrie.playground.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {}
+}
